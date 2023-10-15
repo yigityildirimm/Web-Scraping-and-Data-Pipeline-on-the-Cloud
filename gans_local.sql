c5d0e0e0fbe802dcd21e958260d60f5917e7cf0e
Yigit Yildirim <93987871+yigityildirimm@users.noreply.github.com>
2023-10-15 13:14:53 +0200

    Update gans_local.sql

--- gans_local.sql
+++ gans_local.sql
@@ -3,7 +3,7 @@ CREATE DATABASE gans_local;
 USE gans_local;
 
 CREATE TABLE IF NOT EXISTS cities (
-	city_id INT AUTO_INCREMENT,
+    city_id INT AUTO_INCREMENT,
     city VARCHAR(100),
     latitude FLOAT,
     longitude FLOAT,
@@ -12,14 +12,14 @@ CREATE TABLE IF NOT EXISTS cities (
 
 
 CREATE TABLE IF NOT EXISTS airports(
-	city_id INT NOT NULL,
+    city_id INT NOT NULL,
     icao VARCHAR(10),
     PRIMARY KEY (icao),
     FOREIGN KEY (city_id) REFERENCES cities(city_id)
 );
 
 CREATE TABLE IF NOT EXISTS flights(
-	flight_id INT AUTO_INCREMENT,
+    flight_id INT AUTO_INCREMENT,
     ICAO VARCHAR(10),
     departure_airport VARCHAR(10),
     local_time DATETIME,
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS flights(
 );
 
 CREATE TABLE IF NOT EXISTS weather(
-	weather_id INT AUTO_INCREMENT,
+    weather_id INT AUTO_INCREMENT,
     city_id INT NOT NULL,
     `time` DATETIME,
     temp FLOAT,
